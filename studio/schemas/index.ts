7db7012c15620b4f50289a7cb8682a9f0c1190b3
Egbuta ifeanyi chukwu <egbutaify2@gmail.com>
2026-09-25 08:05:02 +0100

    chore: register LensLink schemas

---
+++ studio/schemas/index.ts
@@ -0,0 +1,15 @@
+import { cameraType } from "./camera";
+import { lensType } from "./lens";
+import { adapterType } from "./adapter";
+import { mountType } from "./mount";
+import { compatibilityRuleType } from "./compatibilityRule";
+import { sourceType } from "./source";
+
+export const schemaTypes = [
+  cameraType,
+  lensType,
+  adapterType,
+  mountType,
+  compatibilityRuleType,
+  sourceType,
+];
