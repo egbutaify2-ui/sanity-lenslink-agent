df9d934deeebe35760f7066cfd49fce41d8cae95
Egbuta ifeanyi chukwu <egbutaify2@gmail.com>
2026-09-25 08:05:13 +0100

    chore: configure Sanity environment

---
+++ web/src/sanity/env.ts
@@ -0,0 +1,8 @@
+export const projectId =
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "kv3pdv23";
+
+export const dataset =
+  process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
+
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2026-05-15";
